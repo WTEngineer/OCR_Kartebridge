9ee8ef7b4a4c9d5c153583a57071803e369c1104
WTEngineer <whitetigertechy@gmail.com>
2025-03-10 13:16:05 +0100

    Ocr update

--- ocr.py
+++ ocr.py
@@ -13,4 +13,21 @@ folder_path = './output/clip/'  # Replace with your folder path
 # List all files in the folder
 image_files = [f for f in os.listdir(folder_path) if f.endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif'))]
 
+# Get the full path for each image
+image_paths = [os.path.join(folder_path, f) for f in image_files]
 
+# Print the list of image paths
+print(image_paths)
+
+# Loop through each image path and process
+for image_path in image_paths:
+    # # Read the image
+    # img = cv2.imread(image_path)
+    
+    # if img is None:
+    #     print(f"Error: Unable to load image {image_path}")
+    #     continue  # Skip this image if it couldn't be loaded
+    
+    mocrtext = mocr(image_path)
+    
+    print(f"Text result is {image_path} - {mocrtext}:")
